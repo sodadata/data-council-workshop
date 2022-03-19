b6fb66b0b6d5519da2cb00dcaf38a4b4013dd75e
Vijay Kiran <mail@vijaykiran.com>
2022-03-19 18:48:55 +0100

    Update script

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-( cd postgres_demo_data ; docker-compose up )
+cd postgres_demo_data/soda-demo-data
+echo "Building docker container ..."
+docker build . -t soda-demo-data
+cd ..
+echo "Starting soda-demo-data container .."
+docker compose up
