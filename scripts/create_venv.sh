d633ad7e4a1ff2153f63653e52447b067ef46878
tombaeyens <mail@tombaeyens.be>
2022-03-22 12:22:08 -0500

    Fixes and more detailed instructions

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -6,7 +6,7 @@ rm -rf .venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-# pip install soda-core-postgres
+pip install soda-core-postgres
 
 echo You now have created a virtual environment with Soda Core and its dependencies in $(pwd)/.venv
 echo Activate the virtual environment with command: source .venv/bin/activate
